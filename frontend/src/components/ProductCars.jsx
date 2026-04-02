0400977a35cd8a6376d402e863e42b7baa9abc5f
anselmo <anselmo.vecchio.jose@gmail.com>
2026-04-02 13:45:24 -0300

    Update

---
+++ frontend/src/components/ProductCars.jsx
@@ -0,0 +1,5 @@
+const ProductCar = () => {
+    
+}
+
+export default ProductCar;
---
+++ frontend/src/components/ProductHouse.jsx
@@ -0,0 +1,40 @@
+import { Product } from '../types/Product';
+import styles from '../style/ProductList.module.css';
+import Image from 'next/image';
+
+const ProductHouse = () => {
+  return (
+    <div>
+      <div className={styles.title}>
+        <h1>Check out all the products we have for you, you&apos;ll miss out!</h1>
+      </div>
+
+      <div className={styles.grid}>
+        {products.map(product => (
+          <div key={product.id} className={styles.card}>
+            <Image
+              src={product.img}
+              alt={product.name}
+              width={200}
+              height={200}
+              className={styles.image}
+            />
+            <h3 className={styles.name}>{product.name}</h3>
+            <p className={styles.price}>${product.price}</p>
+            <button
+  onClick={() => {
+    console.log("ADD", product.id);
+    onAdd(product.id);
+  }}
+  className={styles.button}
+>
+  Add to cart
+</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductHouse;
