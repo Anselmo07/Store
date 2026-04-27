c796c7ae35b0cd152b8901125d98ac9d52cfd5f6
anselmo <anselmo.vecchio.jose@gmail.com>
2026-04-27 20:39:00 -0300

    Update payments

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,9 +4,12 @@ import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
 import { CartController } from './cart/cart.controller';
 import { CartService } from './cart/cart.service';
+import { PaymentsModule } from './payments/payments.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
   controllers: [ProductController, CartController],
   providers: [ProductService, CartService],
+  imports: [PaymentsModule],
 })
 export class AppModule {}
