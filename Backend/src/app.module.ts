7609d244a6678185ca67727e6028baffd55f5f5f
anselmo <anselmo.vecchio.jose@gmail.com>
2026-05-01 11:36:12 -0300

    Update

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,16 +4,11 @@ import { ProductController } from './product/product.controller';
 import { ProductService } from './product/product.service';
 import { CartController } from './cart/cart.controller';
 import { CartService } from './cart/cart.service';
-// import { PaymentsModule } from './modules/payments/payments.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import typeOrmConfig from './config/typeorm';
 
 
 @Module({
   controllers: [ProductController, CartController],
   providers: [ProductService, CartService],
-  imports: [TypeOrmModule.forRootAsync({
-      useFactory: typeOrmConfig,
-    }), ],
+  imports: [],
 })
 export class AppModule {}
